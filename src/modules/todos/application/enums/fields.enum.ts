a76c73002f5a273dd4806e21fec5a44235bfb93e
Pablo Ferrari <pabloferraricaliari@gmail.com>
2025-08-27 19:01:47 -0300

    feat: add application enums for fields and ordering

---
+++ src/modules/todos/application/enums/fields.enum.ts
@@ -0,0 +1,9 @@
+export enum FieldsEnum {
+  id = 'id',
+  name = 'name',
+  category_id = 'category_id',
+  status = 'status',
+  notes = 'notes',
+  created_at = 'created_at',
+  updated_at = 'updated_at',
+}
